10d7cf78da80f1d9c2f7137e02ee8ad780c40f58
Dinushika <dinushikad@nic.lk>
2021-10-28 13:05:13 +0530

    Implement User Service

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -1,8 +1,14 @@
 package services;
 
+import auth.impl.GeneralPasswordHandlerImpl;
+import dao.UserDAO;
+import dao.impl.JDBCUserDAOImpl;
+import dto.UserDTO;
 import exceptions.InputInvalidException;
+import exceptions.UserRegistrationFailedException;
+import util.LogConfig;
 import util.Constants;
-
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,45 +17,104 @@ public class UserService {
     private boolean isExited = false;
     private String loggedInUser = null;
 
-    public void start(){
+    public void start() {
+
         while (!isExited){
             try {
                 int action = selectAction();
                 processUserAction(action);
-            }catch (InputMismatchException | InputInvalidException e){
-                e.printStackTrace();
+            }catch (InputInvalidException  | UserRegistrationFailedException e){
+                System.out.println(e.getMessage());
             }
         }
     }
 
-    private int selectAction() {
-        System.out.print("Select Action\n* Login -> 1\n* Register -> 2\n* List Users -> 3\n* Exit -> 4\n");
+    private int selectAction() throws InputInvalidException {
+
+        System.out.print("\nSelect Action\n" +
+                "* Login -> 1\n" +
+                "* Register -> 2\n" +
+                "* List Users -> 3\n" +
+                "* Exit -> 4\n" +
+                "* Logout -> 5\n");
         Scanner input = new Scanner(System.in);
         int action = -1;
-        action = input.nextInt();
-        while (action != 1 && action != 2 && action != 3 && action != 5) {
-            System.out.println("Enter 1,2 or 3");
+        try {
             action = input.nextInt();
+        }catch (InputMismatchException e){
+            throw new InputInvalidException(Constants.INVALID_INPUT);
+        }
+        while (action != 1 && action != 2 && action != 3 && action != 4 && action != 5) {
+            System.out.println("Enter 1,2,3,4 or 5");
+            try {
+                action = input.nextInt();
+            }catch (InputMismatchException e){
+                throw new InputInvalidException(Constants.INVALID_INPUT);
+            }
+
         }
         return action;
     }
 
-    private void exitUser(){
+    private void exitUser() {
+
         logOutUser();
         isExited = true;
     }
 
     private void logOutUser(){
+
         loggedInUser = null;
     }
 
-    private void processUserAction(int action) throws InputInvalidException {
+    private void processUserAction (int action) throws InputInvalidException, UserRegistrationFailedException {
+
         if(action == Constants.ACTION_EXIT){
             exitUser();
-        }else if(action == Constants.ACTION_LOGOUT){
-            logOutUser();
-        }else if (action == Constants.ACTION_LOGIN){
-            loggedInUser = "dinushika";
+
+        } else if(action == Constants.ACTION_LOGOUT){
+
+            if(isLoggedIn()){
+                logOutUser();
+                System.out.println("Successfully logged out!");
+            } else {
+                System.out.println("You are not logged in!");
+            }
+
+        } else if (action == Constants.ACTION_LOGIN){
+
+            if(isLoggedIn()) {
+                System.out.println(Constants.ALREADY_LOGGED_IN_MESSAGE);
+            } else {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Email: ");
+                String email = input.nextLine();
+                System.out.print("Password: ");
+                String password = input.nextLine();
+
+                boolean isValidUserDetails = validateNewUserDetails(email,password);
+                if(!isValidUserDetails){
+                    throw new InputInvalidException(Constants.USER_REGISTRATION_INVALID_USER_DETAILS);
+                }
+
+                UserDTO existingUser = getUser(email);
+                if(existingUser == null){
+                    LogConfig.makeLog(Constants.FAILED_LOGIN+email);
+                    System.out.println(Constants.CREDENTIALS_INVALID);
+                }else {
+                    GeneralPasswordHandlerImpl passwordHandler = new GeneralPasswordHandlerImpl();
+                    boolean validCredentials = passwordHandler.matchPasswords(password,existingUser.getPassword());
+                    if (validCredentials){
+                        LogConfig.makeLog(Constants.SUCCESS_LOGIN+email);
+                        loggedInUser = existingUser.getEmail();
+                        System.out.println(Constants.LOGGED_IN_MESSAGE);
+                    }else {
+                        LogConfig.makeLog(Constants.FAILED_LOGIN+email);
+                        System.out.println(Constants.CREDENTIALS_INVALID);
+                    }
+                }
+            }
+
         }else if (action == Constants.ACTION_REGISTER){
 
             Scanner input = new Scanner(System.in);
@@ -62,27 +127,66 @@ public class UserService {
             String password = input.nextLine();
 
             boolean isValidUserDetails = validateNewUserDetails(email,name,password);
-
             if(!isValidUserDetails){
-                throw new InputInvalidException(Constants.USER_REGISTRATION_PASSWORD_NOT_MATCHING_MESSAGE);
+                throw new InputInvalidException(Constants.USER_REGISTRATION_INVALID_USER_DETAILS);
             }
 
+            UserDTO existingUser = getUser(email);
+            if(existingUser == null){
+                GeneralPasswordHandlerImpl passwordHandler = new GeneralPasswordHandlerImpl();
+                UserDTO user = new UserDTO(email, name, passwordHandler.encryptPassword(password));
+                boolean isAdded = addUser(user);
+                if(isAdded){
+                    System.out.println(Constants.USER_ADDED_MESSAGE+"\n");
+                } else {
+                    throw new UserRegistrationFailedException(Constants.USER_REGISTRATION_FAILED_MESSAGE);
+                }
+            } else {
+                System.out.println(Constants.USER_EXISTS+"\n");
+            }
 
+        } else if (action == Constants.ACTION_LIST_USERS){
 
-        }else if (action == Constants.ACTION_LIST_USERS){
-
+            if(isLoggedIn()){
+                ArrayList<String> usersList = getAllUsers();
+                System.out.println("\nList of All Users\n");
+                usersList.forEach(value -> System.out.print(value+"\n"));
+            } else {
+                System.out.println("Please login!");
+            }
         }
     }
 
-    private boolean validateNewUserDetails(String... userDetails){
+    private boolean validateNewUserDetails(String... userDetails) {
+
         for (String userDetail : userDetails) {
             if (userDetail.isEmpty()) {
                 return false;
             }
         }
-
         return true;
     }
 
+    private UserDTO getUser(String email) {
 
+        UserDAO userDAO = new JDBCUserDAOImpl();
+        return userDAO.findUser(email);
+    }
+
+    private boolean addUser(UserDTO user){
+
+        UserDAO userDAO = new JDBCUserDAOImpl();
+        return userDAO.addUser(user);
+    }
+
+    private boolean isLoggedIn(){
+
+        return loggedInUser != null;
+    }
+
+    private ArrayList<String> getAllUsers(){
+
+        UserDAO userDAO = new JDBCUserDAOImpl();
+        return userDAO.getAllUsers();
+    }
 }
