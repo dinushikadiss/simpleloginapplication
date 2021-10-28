fc025b42bfdaa382f1024f254da0129cd458dd57
Dinushika <dinushikad@nic.lk>
2021-10-28 13:09:21 +0530

    Implement General Password Handler

---
+++ src/main/java/auth/impl/GeneralPasswordHandlerImpl.java
@@ -0,0 +1,32 @@
+package auth.impl;
+
+import auth.PasswordHandler;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class GeneralPasswordHandlerImpl implements PasswordHandler {
+
+    /**
+     * Encrypts the password
+     *
+     * @param password password needed to be encrypted
+     * @return Encrypted password
+     */
+    @Override
+    public String encryptPassword(String password) {
+
+        return  BCrypt.hashpw( password, BCrypt.gensalt(12) );
+    }
+
+    /**
+     * Compares user entered password with stored password in the database
+     *
+     * @param password entered password
+     * @param hash encrypted password in the database
+     * @return boolean value after comparing two passwords
+     */
+    @Override
+    public boolean matchPasswords(String password, String hash) {
+
+        return  BCrypt.checkpw( password, hash);
+    }
+}
